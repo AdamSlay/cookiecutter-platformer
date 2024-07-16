391c0b8d9e8831d2dc4cf9f203a5f3c640dd0848
Adam Slay <aslay@mesonet.org>
2024-07-16 14:54:05 -0500

    Remove unnecessary #includes from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h>
-#include <nlohmann/json.hpp>
-
-
 #include "utils.h"
 
 
